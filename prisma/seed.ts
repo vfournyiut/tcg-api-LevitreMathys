4fcd8b16a391b81cab07535a48e69c792349d77b
Levitre <levitre.mathys30@gmail.com>
2026-01-29 17:55:40 +0100

    add seed.js

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,10 +8,10 @@ import { PokemonType } from "../src/generated/prisma/enums";
 async function main() {
     console.log("🌱 Starting database seed...");
 
+    await prisma.deckCard.deleteMany();
+    await prisma.deck.deleteMany();
     await prisma.card.deleteMany();
     await prisma.user.deleteMany();
-    await prisma.deck.deleteMany();
-    await prisma.deckCard.deleteMany();
 
     const hashedPassword = await bcrypt.hash("password123", 10);
 
