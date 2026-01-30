1b8189e5ecb60178862685ecdcdb6d0a1a756f5a
Levitre <levitre.mathys30@gmail.com>
2026-01-30 15:45:10 +0100

    First Card's endpoint (list all cards order by numPokemon)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { env } from "./env";
 import express from "express";
 import cors from "cors";
 import { authRouter } from './auth/auth.route';
+import { cardRouter } from './cards/card.route';
 // Create Express app
 export const app = express();
 
@@ -27,6 +28,7 @@ app.get("/api/health", (_req, res) => {
 
 
 app.use("/api/auth", authRouter)
+app.use("/api", cardRouter)
 
 // Start server only if this file is run directly (not imported for tests)
 if (require.main === module) {
