f67d7cbdb7c1e9d2cebebb301e9475e7604e11de
Levitre <levitre.mathys30@gmail.com>
2026-02-05 09:21:26 +0100

    fix: auth route & deps update

--- src/auth/auth.route.ts
+++ src/auth/auth.route.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { Request, Response, Router } from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { prisma } from '../database';
 
---
+++ src/decks/deck.route.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+
+export const deckRouter = Router();
+
+deckRouter.post("/", async (req: Request, res: Response) => {
+    
+})
