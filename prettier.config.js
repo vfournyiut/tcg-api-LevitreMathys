73bf7c3bb1e189351ca4dc11fc5564f8fede5ed6
Levitre <levitre.mathys30@gmail.com>
2026-02-13 09:52:20 +0100

    add: prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,15 @@
-export default {
-  singleQuote: true,
-  trailingComma: 'all',
-  semi: false,
-  printWidth: 80,
-  tabWidth: 2,
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'always',
-  endOfLine: 'lf',
+import type { Config } from 'prettier'
+
+const config: Config = {
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: false,
+    printWidth: 80,
+    tabWidth: 2,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    endOfLine: 'lf',
 }
+
+export default config
